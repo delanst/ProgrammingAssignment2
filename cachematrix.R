a8b62846b6cc8f3084f7f4b8d88027eecae93356
delans1 <Y8HZ1y$CWZPnJ$4s>
2016-03-28 21:40:02 +0200

    Assignment2 Week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+# makeCacheMatrix creates a list containing a function to
+# set the value of the matrix
+# get the value of the matrix
+# set the value of inverse of the matrix
+# get the value of inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) { # sets the matrix and the inv to null (meaning cache is empty)
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x # returns the matrix x
+  setInverse <- function(inverse) inv <<- inverse # cache the inverse within the inv variable in another environment.
+  getInverse <- function() inv # retreive cached value (inv)
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+# The cacheSolve function returns the inverse of the matrix. A check in the cache is
+# performed first and if present then the cache value is returned (so no computation).
+# Otherwise it calculates the inverse and places in the cache.
+# Placing in the cache happens by x$setInverse
+# retrieval from the cache happens by x$getInverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse() # retrieve cached value by returning inv in another environment.
+  if(!is.null(inv)) { # check if cached inverse is present if so return cached value
+    message("getting data from cache.")
+    return(inv)
+  }
+  data <- x$get() # if not cached get the x value 
+  inv <- solve(data, ...) 
+  x$setInverse(inv) # sets the value in the cache.
+  inv # returns the matrix
 }
